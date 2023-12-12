ea7283fbd02eef53eb420ff0ca7f2c0a4d4011c7
SujayAmberkar <sujayamberkar@gmail.com>
2023-12-13 01:05:46 +0530

    Revert "Revert "agent policy implemented""
    This reverts commit 839010b13481721186f9d7cec49f20d69f8598de.

--- Assets/Scripts/RollerAgent.cs
+++ Assets/Scripts/RollerAgent.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
-public class RollerAgent : MonoBehaviour
+
+public class RollerAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody rBody;
+    private void Start()
     {
-        
+        rBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Transform Target;
+    public override void OnEpisodeBegin()
     {
-        
+        // if agent fell, zero it's momentum
+        if(this.transform.position.y < 0)
+        {
+            this.transform.localPosition = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.rotation = Quaternion.identity;
+        }
+
+        // Move target to new random position
+
+        Target.localPosition = new Vector3(Random.value * 8 - 4,
+                                           0,
+                                           Random.value * 8 - 4);
     }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+
 }
